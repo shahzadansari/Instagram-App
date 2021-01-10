3a282bd132c99f314ce2cc46a67bd76df097182d
Shahzad Ansari <shahzadansari03@outlook.com>
2021-01-10 23:52:30 +0500

    Create AccountSettingsActivity

---
+++ app/src/main/java/com/example/instagram_app/AccountSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.instagram_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+    }
+}
--- app/src/main/java/com/example/instagram_app/ProfileFragment.java
+++ app/src/main/java/com/example/instagram_app/ProfileFragment.java
@@ -1,14 +1,18 @@
 package com.example.instagram_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
+
+    private TextView textViewEditProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,6 +27,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        textViewEditProfile = rootView.findViewById(R.id.text_view_edit_profile);
+        textViewEditProfile.setOnClickListener(v -> openAccountSettingsActivity());
+
+        return rootView;
+    }
+
+    private void openAccountSettingsActivity() {
+        Intent intent = new Intent(getActivity(), AccountSettingsActivity.class);
+        startActivity(intent);
     }
 }
