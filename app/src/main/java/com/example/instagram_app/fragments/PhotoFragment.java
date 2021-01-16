c4d994d956dcd1d60f01a4ffd3e7f5fff0b2f65d
Shahzad Ansari <shahzadansari03@outlook.com>
2021-01-16 17:54:45 +0500

    Share Image as Bitmap

--- app/src/main/java/com/example/instagram_app/fragments/PhotoFragment.java
+++ app/src/main/java/com/example/instagram_app/fragments/PhotoFragment.java
@@ -1,15 +1,24 @@
 package com.example.instagram_app.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.instagram_app.R;
 
 public class PhotoFragment extends Fragment {
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private Button buttonOpenCamera;
+    private ImageView imageViewTest;
 
     public PhotoFragment() {
         // Required empty public constructor
@@ -20,6 +29,22 @@ public class PhotoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_photo, container, false);
+        buttonOpenCamera = rootView.findViewById(R.id.button_open_camera);
+
+        buttonOpenCamera.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap bitmap;
+            bitmap = (Bitmap) data.getExtras().get("data");
+        }
     }
 }
--- app/src/main/java/com/example/instagram_app/fragments/ShareFragment.java
+++ app/src/main/java/com/example/instagram_app/fragments/ShareFragment.java
@@ -1,24 +1,41 @@
 package com.example.instagram_app.fragments;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.instagram_app.R;
 import com.example.instagram_app.adapters.FragmentsAdapter;
+import com.example.instagram_app.utils.Permissions;
 import com.google.android.material.tabs.TabLayout;
 
 public class ShareFragment extends Fragment {
 
+    private static final String TAG = "ShareFragment";
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
     public ShareFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (checkPermissionsArray(Permissions.PERMISSIONS)) {
+            Log.d(TAG, "onCreate: checkPermissionsArray: called");
+        } else {
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
     }
 
     @Override
@@ -37,5 +54,35 @@ public class ShareFragment extends Fragment {
     private void initViewPager() {
         viewPager.setAdapter(new FragmentsAdapter(getActivity().getSupportFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public boolean checkPermissionsArray(String[] permissions) {
+
+        for (String permission : permissions) {
+            if (!checkPermissions(permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkPermissions(String permission) {
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(getActivity(), permission);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void verifyPermissions(String[] permissions) {
+
+        ActivityCompat.requestPermissions(
+                getActivity(),
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
     }
 }
