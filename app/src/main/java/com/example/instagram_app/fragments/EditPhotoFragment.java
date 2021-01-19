0fe04148cb202d343d969f86037b5c57709b2eed
Shahzad Ansari <shahzadansari03@outlook.com>
2021-01-19 22:36:12 +0500

    init EditPhotoFragment

--- app/src/main/java/com/example/instagram_app/fragments/EditPhotoFragment.java
+++ app/src/main/java/com/example/instagram_app/fragments/EditPhotoFragment.java
@@ -4,18 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.bumptech.glide.Glide;
 import com.example.instagram_app.R;
 
 public class EditPhotoFragment extends Fragment {
+
     private static final String TAG = "EditPhotoFragment";
     private String imagePath;
-    private ImageView imageView;
+    private ImageView imageView, imageViewBackArrow;
+    private TextView textViewShare;
+    private EditText editTextCaption;
+    private NavController navController;
 
     public EditPhotoFragment() {
         // Required empty public constructor
@@ -31,13 +40,23 @@ public class EditPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_edit_photo, container, false);
-        imageView = rootView.findViewById(R.id.image_view);
+        return inflater.inflate(R.layout.fragment_edit_photo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        imageView = view.findViewById(R.id.image_view);
+        editTextCaption = view.findViewById(R.id.edit_text_caption);
+        imageViewBackArrow = view.findViewById(R.id.image_view_back_arrow);
+        textViewShare = view.findViewById(R.id.text_view_share);
+
+        imageViewBackArrow.setOnClickListener(v -> navController.navigateUp());
 
         Glide.with(getActivity())
                 .load(imagePath)
                 .into(imageView);
-
-        return rootView;
     }
 }
