ea76af46303544e2031533ab7026b9732d7b7974
Shahzad Ansari <shahzadansari03@outlook.com>
2021-01-24 18:50:23 +0500

    init ViewPostFragment widgets

--- app/src/main/java/com/example/instagram_app/fragments/ViewPostFragment.java
+++ app/src/main/java/com/example/instagram_app/fragments/ViewPostFragment.java
@@ -5,15 +5,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.instagram_app.R;
 import com.example.instagram_app.model.Photo;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ViewPostFragment extends Fragment {
 
     private static final String TAG = "ViewPostFragment";
+    private CircleImageView profilePhoto;
+    private ImageView imageViewProfilePhoto, postImage, imageViewEllipses,
+            imageViewChecked, imageViewUnchecked, imageViewComments;
+    private TextView textViewUsername, textViewLikes, textViewCaption,
+            textViewCommentsLink, textViewTimePosted;
+
     private Photo currentPhoto;
 
     public ViewPostFragment() {
@@ -31,6 +42,28 @@ public class ViewPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_post, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_view_post, container, false);
+        imageViewProfilePhoto = rootView.findViewById(R.id.profile_photo);
+        imageViewEllipses = rootView.findViewById(R.id.image_view_ellipses);
+        postImage = rootView.findViewById(R.id.post_image);
+        imageViewChecked = rootView.findViewById(R.id.image_view_heart_checked);
+        imageViewUnchecked = rootView.findViewById(R.id.image_view_heart_unchecked);
+        imageViewComments = rootView.findViewById(R.id.image_view_comments);
+        textViewUsername = rootView.findViewById(R.id.text_view_username);
+        textViewLikes = rootView.findViewById(R.id.text_view_likes);
+        textViewCaption = rootView.findViewById(R.id.text_view_image_caption);
+        textViewCommentsLink = rootView.findViewById(R.id.text_view_image_comments_link);
+        textViewTimePosted = rootView.findViewById(R.id.text_view_time_post_created);
+
+        updateUI();
+        return rootView;
+    }
+
+    private void updateUI() {
+        textViewCaption.setText(currentPhoto.getCaption());
+        textViewTimePosted.setText(currentPhoto.getDate_created());
+        Glide.with(this)
+                .load(currentPhoto.getImage_path())
+                .into(postImage);
     }
 }
