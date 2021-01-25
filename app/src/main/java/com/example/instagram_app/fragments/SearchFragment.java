eb7e4e21b139514070a5733328451567e6a58592
Shahzad Ansari <shahzadansari03@outlook.com>
2021-01-25 23:20:11 +0500

    init SearchFragment

---
+++ app/src/main/java/com/example/instagram_app/fragments/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.example.instagram_app.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.instagram_app.R;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
