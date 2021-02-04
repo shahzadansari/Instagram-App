86114256df3840f77363d6b30d9b749ca1850090
Shahzad Ansari <shahzadansari03@outlook.com>
2021-02-04 12:19:30 +0500

    Display All Users in Search Fragment

---
+++ app/src/main/java/com/example/instagram_app/adapters/SearchUsersAdapter.java
@@ -0,0 +1,82 @@
+package com.example.instagram_app.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.instagram_app.R;
+import com.example.instagram_app.model.User;
+import com.example.instagram_app.model.UserAccountSettings;
+import com.example.instagram_app.model.UserSettings;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SearchUsersAdapter extends ListAdapter<UserSettings, SearchUsersAdapter.ViewHolder> {
+
+    private static final DiffUtil.ItemCallback<UserSettings> DIFF_CALLBACK = new DiffUtil.ItemCallback<UserSettings>() {
+        @Override
+        public boolean areItemsTheSame(UserSettings oldItem, UserSettings newItem) {
+            return oldItem.equals(newItem);
+        }
+
+        @Override
+        public boolean areContentsTheSame(UserSettings oldItem, @NonNull UserSettings newItem) {
+            return oldItem.getUser().getUser_id().equals(newItem.getUser().getUser_id());
+        }
+    };
+    private Context mContext;
+
+    public SearchUsersAdapter(Context context) {
+        super(DIFF_CALLBACK);
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_search_items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        UserSettings userSettings = getItem(position);
+        User user = userSettings.getUser();
+        UserAccountSettings userAccountSettings = userSettings.getSettings();
+
+        Glide.with(mContext)
+                .load(userAccountSettings.getProfile_photo())
+                .into(holder.imageViewProfilePhoto);
+
+        holder.textViewUsername.setText(user.getUsername());
+        holder.textViewDisplayName.setText(userAccountSettings.getDisplay_name());
+    }
+
+    // TODO: add a field "commentId" in Comment model, to delete comments with.
+    public UserSettings getUserSettingsAt(int position) {
+        return getItem(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView imageViewProfilePhoto;
+        TextView textViewUsername;
+        TextView textViewDisplayName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewProfilePhoto = itemView.findViewById(R.id.search_result_image);
+            textViewUsername = itemView.findViewById(R.id.search_result_username);
+            textViewDisplayName = itemView.findViewById(R.id.search_result_display_name);
+        }
+    }
+}
--- app/src/main/java/com/example/instagram_app/fragments/SearchFragment.java
+++ app/src/main/java/com/example/instagram_app/fragments/SearchFragment.java
@@ -1,24 +1,122 @@
 package com.example.instagram_app.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.instagram_app.R;
+import com.example.instagram_app.adapters.SearchUsersAdapter;
+import com.example.instagram_app.model.User;
+import com.example.instagram_app.model.UserAccountSettings;
+import com.example.instagram_app.model.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
+
+    private EditText editText;
+    private RecyclerView recyclerViewSearchResults;
+    private SearchUsersAdapter adapter;
+
+    private Context mContext;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+
+    private List<User> usersList = new ArrayList<>();
+    private List<UserAccountSettings> userAccountSettingsList = new ArrayList<>();
+    private List<UserSettings> usersSettingsList = new ArrayList<>();
 
     public SearchFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        recyclerViewSearchResults = rootView.findViewById(R.id.recycler_view_search_results);
+        editText = rootView.findViewById(R.id.edit_text_search);
+
+        initEmptyRecyclerView();
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                getUsersSettings(snapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initEmptyRecyclerView() {
+        adapter = new SearchUsersAdapter(mContext);
+        recyclerViewSearchResults.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager
+                (mContext, LinearLayoutManager.VERTICAL, false);
+
+        recyclerViewSearchResults.setLayoutManager(linearLayoutManager);
+    }
+
+    private void getUsersSettings(DataSnapshot snapshot) {
+
+        for (DataSnapshot dataSnapshot : snapshot.child("users")
+                .getChildren()) {
+            User user = dataSnapshot.getValue(User.class);
+            usersList.add(user);
+        }
+
+        for (DataSnapshot dataSnapshot : snapshot.child("user_account_settings")
+                .getChildren()) {
+            UserAccountSettings userAccountSettings = dataSnapshot.getValue(UserAccountSettings.class);
+            userAccountSettingsList.add(userAccountSettings);
+        }
+
+        for (int i = 0; i < usersList.size(); i++) {
+            UserSettings userSettings = new UserSettings(usersList.get(i), userAccountSettingsList.get(i));
+            usersSettingsList.add(userSettings);
+        }
+
+        adapter.submitList(usersSettingsList);
     }
 }
